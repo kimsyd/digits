2f1f32a15e44c8bcb9b131ed0342708ba93db844
kimsyd <kimsyd@hawaii.edu>
2023-10-31 13:36:42 -1000

    contacts collection finished in 31 minutes

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,18 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Contacts } from '../../api/stuff/Contacts.js';
 
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
 const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
+  console.log(`  Adding: ${data.firstName} ${data.lastName} ${data.address}  ${data.image} ${data.description} (${data.owner})`);
+  Contacts.collection.insert(data);
 };
 
 // Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
+if (Contacts.collection.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating default contacts.');
+    Meteor.settings.defaultContacts.forEach(data => addData(data));
   }
 }
--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
-import Contact from '../components/Contact';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Contacts } from '../../api/stuff/Contacts';
+import ContactAdmin from '../components/ContactAdmin';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const ListContacts = () => {
+const ListContactsAdmin = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   // eslint-disable-next-line no-unused-vars
   const { ready, contacts } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Contacts.userPublicationName);
+    const subscription = Meteor.subscribe(Contacts.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
@@ -29,12 +29,12 @@ const ListContacts = () => {
       <Row className="justify-content-center">
         <Col>
           <Col className="text-center">
-            <h2>List Contacts</h2>
+            <h2>List Contacts (Admin)</h2>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
             {contacts.map((contact, index) => (
               <Col key={index}>
-                <Contact contact={contact} />
+                <ContactAdmin contact={contact} />
               </Col>
             ))}
           </Row>
@@ -43,4 +43,4 @@ const ListContacts = () => {
     </Container>
   ) : <LoadingSpinner />);
 };
-export default ListContacts;
+export default ListContactsAdmin;
